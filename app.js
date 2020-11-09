7781dedb214a2b3b89a9355a0d0a7bc031423e3e
sebastians9517 <sebastians9517@gmail.com>
2020-11-08 21:08:48 -0600

    guess the number homework

--- app.js
+++ app.js
@@ -13,122 +13,90 @@
 // Display an alert message that congrats the player and informs them of how many guesses they took.
 // End the gameplay.
 
-// const game = {
-//     title: 'Guess the number',
-//     biggestNum: 10,
-//     smallestNum: 1,
-//     prevGuesses: [],
-//     currentGuess: null,
-//     getGuess: function () {
-//         this.currentGuess = prompt(`Enter a guess between ${this.game.smallestNum} and ${this.game.biggestNum}:`);
-//     },
-//     get getGuess() {
-//         return this.getGuess;
-//     },
-//     set getGuess(value) {
-//         this.getGuess = value;
-//     },
-//     secretNum: null,
-//     play: function(){
-//         this.secretNum = Math.floor(Math.random() *
-//         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+// // const game = {
+// //     title: 'Guess the number',
+// //     biggestNum: 100,
+// //     smallestNum: 1,
+// //     prevGuesses: [],
+// //     currentGuess: null,
+// //     getGuess: function () {
+// //         this.currentGuess = prompt(`Enter a guess between ${this.game.smallestNum} and ${this.game.biggestNum}:`);
+// //     },
+// //     get getGuess() {
+// //         return this.getGuess;
+// //     },
+// //     set getGuess(value) {
+// //         this.getGuess = value;
+// //     },
+// //     secretNum: null,
+// //     play: function(){
+// //         this.secretNum = Math.floor(Math.random() *
+// //         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
 
-//         return this.secretNum; //function here that randomizes my range
-//     }
-// };
-
-// // while (currentGuess != secretNum) {
-// //         alert.game.getGuess;
-// //         this.prevGuesses.push(this.currentGuess);
+// //         return this.secretNum; // //function here that randomizes my range
 // //     }
+// // };
+
+// // // // while (currentGuess != secretNum) {
+// // // //         alert.game.getGuess;
+// // // //         this.prevGuesses.push(this.currentGuess);
+// // // //     }
 
 
-// console.log(game.getGuess());
-// console.log(game.play());
+// // console.log(game.getGuess());
+// // console.log(game.play());
 
-// const game = {
-//     title: 'Guess the number',
-//     biggestNum: 100,
-//     smallestNum: 1,
-//     prevGuesses: [],
-//     currentGuess: null,
-//     secretNum: null,
-//     getGuess: function () {
-//         this.currentGuess = prompt(`Enter a guess between ${this.game.smallestNum} and ${this.game.biggestNum}:`);
-//         let number = parseInt(this.currentGuess);
-//         while (this.currentGuess !== isNaN(number) && this.currentGuess >= smallestNum && this.currentGuess <= biggestNum){
-//             prompt(`Enter a natural number between ${this.game.smallestNum} and ${this.game.biggestNum}`);
+// // const game = {
+// //     title: 'Guess the number',
+// //     biggestNum: 100,
+// //     smallestNum: 1,
+// //     prevGuesses: [],
+// //     currentGuess: null,
+// //     secretNum: null,
+// //     getGuess: function () {
+// //         this.currentGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`);
+// //         let number = parseInt(this.currentGuess);
+// //         while (this.currentGuess !== isNaN(number) || this.currentGuess >= this.smallestNum || this.currentGuess <= this.biggestNum){
+// //             prompt(`Enter a natural number between ${this.smallestNum} and ${this.biggestNum}`);
 
-//             if (this.currentGuess > this.secretNum){
-//                 alert(`Your number is too high`);
-//                 this.prevGuesses.push(this.currentGuess);
-//             } else if (this.currentGuess < this.secretNum) {
-//                 alert(`Your number is too low`);
-//                 this.prevGuesses.push(this.currentGuess);
-//             } else if (this.currentGuess == this.secretNum) {
-//                 alert(`Congratulations! You guessed the number! in ${this.prevGuesses.length} attempts!
-//                 Your guesses where ${this.prevGuesses.join}`);
-//             } else
-//                 alert(`This is not a number.`);
-//         }
-//     },
-//     play: function() {
-//         this.secretNum = Math.floor(Math.random() *
-//         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-//         console.log(this.secretNum);
-//         this.getGuess();
-//     },
+// //             if (this.currentGuess > this.secretNum){
+// //                 alert(`Your number is too high`);
+// //                 this.prevGuesses.push(this.currentGuess);
+// //             } else if (this.currentGuess < this.secretNum) {
+// //                 alert(`Your number is too low`);
+// //                 this.prevGuesses.push(this.currentGuess);
+// //             } else if (this.currentGuess == this.secretNum) {
+// //                 alert(`Congratulations! You guessed the number! in ${this.prevGuesses.length} attempts!
+// //                 Your guesses where ${this.prevGuesses.join}`);
+// //             } else
+// //                 alert(`This is not a number.`);
+// //         }
+// //     },
+// //     play: function() {
+// //         this.secretNum = Math.floor(Math.random() *
+// //         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+// //         console.log(this.secretNum);
+// //         this.getGuess();
+// //     },
 
 
-// }
-// game.play();
-// const name = prompt('Enter your name: ');
-// const age = prompt('Enter your age: ');
-// const person = {name, age};
+// // }
+// // game.play();
 
-//  NOTE TO INSTRUCTOR
-// I could not make the object work at all. Kept saying that smallestNum and prompt, etc were not defined and I just couldn't find a way around it. Therefore I wrote the game as a function, which works decently. Below I tried to write this same function as an object, and it kept giving me the same errors.
 
-function guessTheNumberGame() {
-    let smallestNum = 1;
-    let biggestNum = 100;
-    let playerGuess = parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`));
-    let attempts = [];
-    let secretNum = Math.floor(Math.random() * (biggestNum - smallestNum + 1)) + smallestNum;
-    while (playerGuess !== secretNum && playerGuess >= smallestNum && playerGuess <= biggestNum) {
-        playerGuess = parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`))
 
-        if (playerGuess > secretNum) {
-            alert(`This number is too high. Please try entering a lower number.`);
-            attempts.push(playerGuess);
-        }
-        else if (playerGuess < secretNum) {
-            alert(`This number is too low. Please try entering a higher number.`);
-            attempts.push(playerGuess);
-        }
-        else if (playerGuess == secretNum) {
-            attempts.push(playerGuess);
-            alert(`CONGRATULATIONS! You guessed the right number in ${attempts.length} attempts, which were ${attempts.join()}`);
-        }
-        else if (isNaN(playerGuess)) {
-            alert(`This is not a number. Please enter a natural number between ${smallestNum} and ${biggestNum}`);
-        }
-        else alert(`Your number is out of the stablished range. Please enter a number between ${smallestNum} and ${biggestNum}`);
-    };
-};
+// //  NOTE TO INSTRUCTOR
+// // I could not make the object work at all. Kept saying that smallestNum and prompt, etc were not defined and I just couldn't find a way around it. Therefore I wrote the game as a function, which works decently. Below I tried to write this same function as an object, and it kept giving me the same errors.
 
-guessTheNumberGame();
+// function guessTheNumberGame() {
+//     let smallestNum = 1;
+//     let biggestNum = 100;
+//     let playerGuess = parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`));
+//     let attempts = [];
+//     let secretNum = Math.floor(Math.random() * (biggestNum - smallestNum + 1)) + smallestNum;
+//     while (playerGuess !== secretNum && playerGuess >= smallestNum && playerGuess <= biggestNum) {
+//         playerGuess = parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`))
 
-// const guessTheNumberGame = {
-//     smallestNum 1,
-//     biggestNum: 100,
-//     playerGuess: parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`)),
-//     attempts: [],
-//     secretNum: Math.floor(Math.random() * (biggestNum - smallestNum + 1)) + smallestNum,
-
-//     play: function() {
-//         while (playerGuess !== secretNum && playerGuess >= smallestNum && playerGuess <= biggestNum) {
-//             this.playerGuess
 //         if (playerGuess > secretNum) {
 //             alert(`This number is too high. Please try entering a lower number.`);
 //             attempts.push(playerGuess);
@@ -139,12 +107,134 @@ guessTheNumberGame();
 //         }
 //         else if (playerGuess == secretNum) {
 //             attempts.push(playerGuess);
-//             alert(`CONGRATULATIONS! You guessed the right number in ${attempts.length} attempts, which were ${attempts.join}`);
+//             alert(`CONGRATULATIONS! You guessed the right number in ${attempts.length} attempts, which were ${attempts.join()}`);
 //         }
 //         else if (isNaN(playerGuess)) {
 //             alert(`This is not a number. Please enter a natural number between ${smallestNum} and ${biggestNum}`);
 //         }
 //         else alert(`Your number is out of the stablished range. Please enter a number between ${smallestNum} and ${biggestNum}`);
 //     };
-// },
-// }
+// };
+
+// guessTheNumberGame();
+
+
+
+
+// // const guessTheNumberGame = {
+// //     smallestNum: 1,
+// //     biggestNum: 100,
+// //     playerGuess: parseInt(prompt(`Please enter a natural number between ${smallestNum} and ${biggestNum}`)),
+// //     attempts: [],
+// //     secretNum: Math.floor(Math.random() * (biggestNum - smallestNum + 1)) + smallestNum,
+
+// //     play: function() {
+// //         while (playerGuess !== secretNum && playerGuess >= smallestNum && playerGuess <= biggestNum) {
+// //             this.playerGuess
+// //         if (playerGuess > secretNum) {
+// //             alert(`This number is too high. Please try entering a lower number.`);
+// //             attempts.push(playerGuess);
+// //         }
+// //         else if (playerGuess < secretNum) {
+// //             alert(`This number is too low. Please try entering a higher number.`);
+// //             attempts.push(playerGuess);
+// //         }
+// //         else if (playerGuess == secretNum) {
+// //             attempts.push(playerGuess);
+// //             alert(`CONGRATULATIONS! You guessed the right number in ${attempts.length} attempts, which were ${attempts.join}`);
+// //         }
+// //         else if (isNaN(playerGuess) == true) {
+// //             alert(`This is not a number. Please enter a natural number between ${smallestNum} and ${biggestNum}`);
+// //         }
+// //         else alert(`Your number is out of the stablished range. Please enter a number between ${smallestNum} and ${biggestNum}`);
+// //     };
+// // },
+// // }
+
+
+
+
+
+
+
+// // const game = {
+// //     title: 'Guess the number',
+// //     biggestNum: 100,
+// //     smallestNum: 1,
+// //     prevGuesses: [],
+// //     currentGuess: null,
+// //     secretNum: null,
+// //     getGuess: function () {
+// //       while (isNaN(this.currentGuess) ||
+// //       this.currentGuess >= this.smallestNum ||
+// //       this.currentGuess <= this.biggestNum){
+// //         this.currentGuess = parseInt(prompt(`Enter a guess between ${thissmallestNum} and ${this.biggestNum}:`));
+// //         if (this.currentGuess > this.secretNum){
+// //           alert(`Your number is too high`);
+// //           this.prevGuesses.push(this.currentGuess);
+// //         } else if (this.currentGuess < this.secretNum) {
+// //           alert(`Your number is too low`);
+// //           this.prevGuesses.push(this.currentGuess);
+// //         } else if (this.currentGuess == this.secretNum) {
+// //           alert(`Congratulations! You guessed the number! in ${thisprevGuesses.length} attempts!
+// //           Your guesses where ${this.prevGuesses.join}`);
+// //         } else
+// //           alert(`This is not a number.`);
+// //       }
+// //     },
+// //     play: function() {
+// //       this.secretNum = Math.floor(Math.random() *
+// //       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+// //       console.log(this.secretNum);
+// //       this.getGuess();
+// //     },
+// //   }
+
+// //   game.play()
+
+
+const game = {
+    name: 'Guess the number',
+    biggestNum: 100,
+    smallestNum: 1,
+    randomNum: null,
+    playerGuess: null,
+    attempts: [],
+    attemptPopulate: function () {this.attempts.push(this.playerGuess)},
+    getGuess: function() {
+        this.playerGuess = parseInt(prompt(`Please enter a natural number between ${this.smallestNum} and ${this.biggestNum}`));
+
+        while (this.playerGuess.isNaN ||
+            this.playerGuess >= this.smallestNum ||
+            this.playerGuess <= this.biggestNum) {
+             this.playerGuess = parseInt(prompt(`Please enter a natural number between ${this.smallestNum} and ${this.biggestNum}`));
+
+                if (this.playerGuess == this.randomNum) {
+                    this.attemptPopulate();
+                    alert(`CONGRATULATIONS! You guessed the right number in ${this.attempts.length} attempts, which were ${this.attempts.join()}`);
+                }
+                else if (this.playerGuess <= this.randomNum) {
+                    this.attemptPopulate();
+                    alert(`Oh no! It seems your number is too low, please try entering a higher number!`);
+                }
+                else if (this.playerGuess >= this.randomNum) {
+                    this.attemptPopulate();
+                    alert(`This number seems to be too high, please try entering a lower number`);
+                }
+                else {
+                    this.attemptPopulate();
+                    alert(`You entered something out of the stablished range. Please enter a natural number between ${this.smallestNum} and ${this.biggestNum}`);
+            }
+        };
+    },
+    play: function() {
+
+        this.randomNum = Math.floor(Math.random() *
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+        console.log(this.randomNum);
+        this.getGuess();
+    },
+
+}
+
+game.play();
